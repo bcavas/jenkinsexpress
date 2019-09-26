da5fff62d35c2e9cf22f05ef2a843a97b34fcb14
Benjamin Cavas <ben.cavas@tuitt.com>
2019-09-26 16:13:54 +0800

    update port to reflect environment variable for deploying in heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
 	res.send('hello')
 })
 
-app.listen(port, ()=>{
+app.listen(PORT, ()=>{
 	console.log(`example app listening on port ${port}!`)
 })
